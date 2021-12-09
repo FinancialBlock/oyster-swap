da9bca39d3907753a8e0fe289a849126a381ea70
FinancialBlock <financialblockchain@gmail.com>
2021-12-09 17:14:05 -0600

    Updates

---
+++ precache-manifest.643985d754b4911508402b31498fc688.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad249fb9cd82c6c512eab0fccbeb9f97",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a1a606d746627bf3d65",
+    "url": "./static/css/2.4b60f7c3.chunk.css"
+  },
+  {
+    "revision": "ccf756e4274c296c4336",
+    "url": "./static/css/main.a092e24b.chunk.css"
+  },
+  {
+    "revision": "8a1a606d746627bf3d65",
+    "url": "./static/js/2.267b8a0f.chunk.js"
+  },
+  {
+    "revision": "af943e5d863be82c882446baf7b4c3b1",
+    "url": "./static/js/2.267b8a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf756e4274c296c4336",
+    "url": "./static/js/main.2243b5e9.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  },
+  {
+    "revision": "c8a93a9a80452cc97730938ebe26a1cb",
+    "url": "./static/media/facxlogo.c8a93a9a.png"
+  }
+]);
